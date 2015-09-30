aeb23abdb39b26ffffbd86a534dbca7ba26c6bf4
David Churchill <dave.churchill@gmail.com>
2015-09-30 14:42:19 -0600

    typo fix

--- src/client/Client.java
+++ src/client/Client.java
@@ -355,7 +355,7 @@ public class Client extends Thread
 			}
 			
 			// Start loader and starcraft
-			ClientCommands.Client_StartChaosLauncher();
+			ClientCommands.Client_StartChaoslauncher();
 
 			// Record the time that we tried to start the game
 			startTime = System.currentTimeMillis();
