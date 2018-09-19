a5f6d5e97e4c0cb16f3f4b250e4a0c207476810f
Rick Kelly <z24rmk@mun.ca>
2018-09-19 13:48:35 -0230

    fix status message for ladder branch

--- src/objects/ClientStatusMessage.java
+++ src/objects/ClientStatusMessage.java
@@ -27,6 +27,6 @@ public class ClientStatusMessage implements Message
 	
 	public String toString()
 	{
-		return "Game " + game.getGameID() + " status: " + status.toString();
+		return status.toString();
 	}
 }
